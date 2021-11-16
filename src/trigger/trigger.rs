d4a16c72a88b38a3bfd52febda73619ef4a9d003
skilzaa <skillzaa.com@gmail.com>
2021-11-16 14:08:21 +0500

    idea about better org of the app-- why is trigger responsible for giving spiderpack and running trigger, it shd be moved to upper items and keep trigger just a firring machine

--- src/trigger/trigger.rs
+++ src/trigger/trigger.rs
@@ -4,6 +4,13 @@ use super::At;
 use crate::SpiderErrors;
 use crate::SpiderPack;
 // #[derive(Debug)]
+/// The Trigger Struct look_for any given pattern with 
+/// in a line.
+/// Keep in mind that if it finds a pattern (as per the 
+/// look_for property) **Twice** in one line, it will 
+/// trigger just once. This will happen with the 
+/// At::Anywhere clause. 
+/// The execute method will strip empty space from a line end and start thus **Do not form triggers based on empty spaces"
 pub struct Trigger {
     name: String,
     look_for:String,
@@ -39,7 +46,9 @@ impl Trigger {
         }
     }
     fn anywhere(&self, line:&String)->bool{
-        true
+        if line.contains(&self.look_for){
+            true
+        }else{false}
     }    
     fn run_trigger(&self,line:&String,spider_pack:&mut SpiderPack)->Result<bool,SpiderErrors>{
             match (self.pointer)(spider_pack) {
@@ -50,23 +59,26 @@ impl Trigger {
             }
     }
     pub fn execute(&self,line:&String,spider_pack:&mut SpiderPack)->Result<bool,SpiderErrors>{
+        let line = String::from(line.trim());
         match self.at {
             At::LineStart=>{
-                if self.line_start(line){
+                if self.line_start(&line){
                     let r = 
-                    self.run_trigger(line,spider_pack);
+                    self.run_trigger(&line,spider_pack);
                     r
                 }else {Ok(false)}//means no trigger
             },
             At::LineEnd=>{
-                if self.line_end(line){
-                    let r = self.run_trigger(line,spider_pack);
+                if self.line_end(&line){
+                    let r 
+                    = self.run_trigger(&line,spider_pack);
                     r
                 }else{Ok(false)}//means no trigger
             },
             At::Anywhere=>{
-                if self.anywhere(line){
-                    let r = self.run_trigger(line,spider_pack);
+                if self.anywhere(&line){
+                    let r 
+                    = self.run_trigger(&line,spider_pack);
                     r
                 }else{Ok(false)}//means no trigger
             },
@@ -116,6 +128,85 @@ use super::*;
 
     let res = trig.line_end(&String::from("some text with"));
     assert_eq!(res,false);    
+
+    }
+    #[test]
+    fn anywhere_one(){
+        //it has extra space at the end
+        let trig = Trigger::new("second",">",
+            At::Anywhere,eve_handle_fn);
+
+    let res = trig.anywhere(&String::from("some >text with"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from(">some text with"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from("some text with>"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from("some> text with>"));
+    assert_eq!(res,true);    
+
+    }
+}//mod tests
+#[cfg(test)]
+mod execute {
+
+use super::*;
+    #[test]
+    fn trig_line_start() {
+        let tri = Trigger::new("new_trig",
+        "!", At::LineStart,eve_handle_fn );
+            assert_eq!(tri.name,"new_trig");
+            assert_eq!(tri.look_for,"!");
+            //line begin with SPACE -shd be ok
+    let line_start_result = 
+    tri.execute(&String::from(" ! some thing"));
+    assert_eq!(line_start_result,true);    
+    let line_start_result02 = 
+    tri.line_start(&String::from("s!ome thing"));
+    assert_eq!(line_start_result02,false);
+    //space added at start    
+    let line_start_result03 = 
+    tri.line_start(&String::from(" !!!ome thing"));
+    assert_eq!(line_start_result03,true);   
+    } 
+    // This is open trigger fn for all tests        
+    fn eve_handle_fn (_spider_pack:&mut SpiderPack)->bool {
+        println!("eve_handle_fn was run....!!!!");
+        true
+    }
+    #[test]
+    fn trig_line_end(){
+        let trig = Trigger::new("second",">",
+            At::LineEnd,eve_handle_fn);
+
+    let res = trig.line_end(&String::from("some text with >"));
+    assert_eq!(res,true);    
+
+    }
+    #[test]
+    fn trig_line_end02(){
+        //it has extra space at the end
+        let trig = Trigger::new("second",">",
+            At::LineEnd,eve_handle_fn);
+
+    let res = trig.line_end(&String::from("some text with"));
+    assert_eq!(res,false);    
+
+    }
+    #[test]
+    fn anywhere_one(){
+        //it has extra space at the end
+        let trig = Trigger::new("second",">",
+            At::Anywhere,eve_handle_fn);
+
+    let res = trig.anywhere(&String::from("some >text with"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from(">some text with"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from("some text with>"));
+    assert_eq!(res,true);    
+    let res = trig.anywhere(&String::from("some> text with>"));
+    assert_eq!(res,true);    
 
     }
 }//mod tests
