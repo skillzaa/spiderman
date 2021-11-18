a5825e2737ea47fc2d815ce185b96837418b54e6
skilzaa <skillzaa.com@gmail.com>
2021-11-18 16:54:28 +0500

    Maan gay rust ko

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
 # spiderman - An event based string parser.
 2021-11-12
 ---
-> Purpose
+> Spiderman can be used as a simple parser or can be used as a framework for building your own parsers.
+---
+## Basic Idea:
+The spiderman scan the provided text line by line (in future I may add word by word scans as well).
+Every line is checked according to **Triggers** created by the user. 
+Once such a line is found a function (*even handler function*) that is provided by the user is run.
+In addition to Triggers the user also needs to create **Records** (as many as user wish) to record different parts of the string.
+The **even handler** function gets a "bag of goodies" called **SpiderPack**. This pack has all the user recorders which he has created. 
+The **SpiderPack** also has other items like **Flags** etc.
+When we *Start a Recorder* it starts recording the lines being scanned.
+When we **Stop the Recorder** the lines being scanned are skipped by the recorder.
+
+## How It Works 
+- We provide string to the SpiderMan.
+- We create **Triggers** (events) for each line. A trigger needs 4 main informartions:
+  1. Name of the trigger (to identify it later).
+  2. What to look for (look_for) in each line.
+  3. Where to look at (At::LineStart,At::LineEnd,At::Anywhere )
+  4. The name of the function to run incase an event is fired (i.e such a line is found).
+- We also creat **Records** to record portions of the text being scanned.
+- When a trigger happens it has the pointer to an event handler function, which is executed.
+- The function that you provide as **event handler** should have the following signature and should always return true. Incase it fails to return true the process will stop.
+``` rust
+fn(spider_pack:&mut SpiderPack)->bool
+```
+*For the time being spiderman just uses line by line scan of the string provided. In future I may add word by word iteration also.*
 
 ---
-> The API
+# Example
 ---
-- SPIDERMSN
- <!-- Trigger Syntax
- let trig = spider.triggers.add("trigger_name)
- .look_for("##").at().line_end();  
-  -->
 
----
+### Notes for me
+- the auto_append is seperate from **every** function. The **every** event handling call on everyline only when the recorder is start.
+- once a recorder is start
+  - it can auto append 
+  - AND
+  - use the every function.
+  - it can do both which ever the user want.
+  - The default behaviour of auto append is true. but as long as the record is start.!!!!
+- the user can Also not append unless the recorder is start.
+
+- Just like **every** there is an **eof** fn with every recorder that will be run at the end of file.
+
+## New Features
+1. recorder :: Skip 1,2,3 for the recorder
+1. recorder :: auto_append
+1. recorder :: every function to recorder
+1. recorder :: eof function to recorder
+1. recorder :: write to file
+1. recorder :: skip white lines: this flag will not append empty lines during auto_append.
--- src/recorders/recorder.rs
+++ src/recorders/recorder.rs
@@ -1,8 +1,18 @@
-
+// This worked 
+use crate::spiderpack::SpiderPack;
+// but not this  
+// use crate::spiderpack; 
+// since in lib.rs only spiderpack::SpiderPack is inscope and not spiderpack
 pub struct Recorder {
     name:String,
     data:String,
-    flag:bool,
+    mode:bool,
+    write_to_file:bool,
+    skip_empty_lines:bool,
+    skip:u32,
+    auto_append:bool,
+    every_line: Option<fn(SpiderPack)->bool>,
+    eof: Option<fn(SpiderPack)->bool>,
 }
 
 impl Recorder {
@@ -11,20 +21,26 @@ impl Recorder {
         Recorder {
             name: String::from(name),
             data: String::from(""),
-            flag:false,
+            mode:false,
+            write_to_file:false,
+            skip_empty_lines:false,
+            auto_append:true,
+            skip:0,
+            every_line: None,
+            eof: None,
         }
     }
     /// The start fn will make the recorder recording all the 
     pub fn start(&mut self)->bool{
-        self.flag = true; 
-        self.flag
+        self.mode = true; 
+        self.mode
     }
     pub fn stop(&mut self)->bool{
-        self.flag = false;
-        self.flag
+        self.mode = false;
+        self.mode
     }
     pub fn is_start(&self)->bool{
-        self.flag
+        self.mode
     }
     pub fn clear(&mut self){
         self.data = String::from("");
@@ -39,7 +55,7 @@ impl Recorder {
     /// recorder is open AND the append goes 
     /// successfully. 
     pub fn append(&mut self,data:&String)->bool{
-        if self.flag {
+        if self.mode {
             self.data.push_str(&data);
             return true;
        }
@@ -48,6 +64,50 @@ impl Recorder {
     /// The copy fn will return a copy of the data from the recorder 
     /// buffer.*It is a copy not a reference to the original 
     /// buffer*
+    pub fn every_line(&mut self,
+    event_handler : fn(SpiderPack)->bool ){
+        self.every_line = Some(event_handler);
+    }
+    pub fn eof(&mut self,
+    event_handler : fn(SpiderPack)->bool ){
+        self.eof = Some(event_handler);
+    }
+    pub fn skip(&mut self,number_of_lines:u32)->u32{
+        self.skip = number_of_lines;
+        self.skip
+    }
+    fn skip_minus_one(&mut self)->u32{
+        if self.skip >= 1 {
+            self.skip = self.skip - 1;
+            self.skip
+        }else{
+            self.skip
+        } 
+    }
+    pub fn get_skip(&self)->u32{
+        self.skip
+    }
+    pub fn auto_append(&mut self,true_false:bool)->bool{
+        self.auto_append = true_false;
+        self.auto_append
+    }
+    pub fn get_auto_append(&self)->bool{
+        self.auto_append
+    }
+    pub fn skip_empty_lines(&mut self,true_false:bool)->bool{
+        self.skip_empty_lines = true_false;
+        self.skip_empty_lines
+    }
+    pub fn get_skip_empty_lines(&self)->bool{
+        self.skip_empty_lines
+    }
+    pub fn write_to_file(&mut self,true_false:bool)->bool{
+        self.write_to_file = true_false;
+        self.write_to_file
+    }
+    pub fn get_write_to_file(&self)->bool{
+        self.write_to_file
+    }
     pub fn copy(&self)->String{
         let copy = self.data.clone();
         copy
